3c69a00b9f920dae7fa91e2227eeb4bcc52eae79
ShhabPalekar <ShhabPalekar23@gmail.com>
2025-11-29 16:15:46 +0530

    count letters in a string - multiple scenarios

--- count-letters-in-string/solution.js
+++ count-letters-in-string/solution.js
@@ -1,3 +1,7 @@
+//SCEANRIO 1
+//return count
+
+//Solution 1
 function count(string) {
   const count = {};
 
@@ -12,7 +16,6 @@ function count(string) {
   return count;
 }
 
-
 //Solution 2 (using array.reduce)
 function count(string) {
   return string.split("").reduce(function (counts, char) {
@@ -20,3 +23,100 @@ function count(string) {
     return counts;
   }, {});
 }
+
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+//SCENARIO 2
+//return most repeated letter (first most repeated letter)
+function mostRepeated(str) {
+  const count = {};
+
+  str.split("").forEach(i => {
+    count[i] = (count[i] || 0) + 1
+  })
+
+  let highestCount = 0
+  let mostRepeatedLetter = null
+  
+  for(let key in count){
+    if(count[key] > highestCount){
+      highestCount = count[key]
+      mostRepeatedLetter = key
+    }
+  }
+  
+  return mostRepeatedLetter
+}
+
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+//SCENARIO 3
+//return most repeated letter (last most repeated letter)
+function mostRepeated(str) {
+  const count = {};
+
+  str.split("").forEach(i => {
+    count[i] = (count[i] || 0) + 1
+  })
+
+  let highestCount = 0
+  let mostRepeatedLetter = null
+  
+  for(let key in count){
+    if(count[key] >= highestCount){
+      highestCount = count[key]
+      mostRepeatedLetter = key
+    }
+  }
+  
+  return mostRepeatedLetter
+}
+
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+//SCENARIO 4
+//return most repeated letter (array of all most repeated letters)
+function mostRepeated(str) {
+  const count = {};
+
+  str.split("").forEach(i => {
+    count[i] = (count[i] || 0) + 1
+  })
+
+  let highestCount = 0
+  
+  for(let key in count){
+    if(count[key] > highestCount){
+      highestCount = count[key]
+    }
+  }
+
+  const mostRepeatedLetters = [];
+  for (let key in count) {
+    if (count[key] === highestCount) {
+      mostRepeatedLetters.push(key);
+    }
+  }
+  
+  return mostRepeatedLetters
+}
+
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+//SCENARIO 5
+//return count of letters in descending order
+
+function mostRepeated(str) {
+  const count = {};
+
+  str.split("").forEach(i => {
+    count[i] = (count[i] || 0) + 1
+  })
+
+ const sorted = Object.entries(count)
+    .sort((a, b) => b[1] - a[1]);
+    
+    return Object.fromEntries(sorted)
+}
+
+/*----------------------------------------------------------------------------------------------------------------------------*/
